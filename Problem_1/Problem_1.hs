3f12dedee6c2241646c33ed7adf260deca3bfddb
Edd <hello@edwardhaigh.com>
2016-10-10 13:56:25 +0100

    Re-wrote Problem_1 in Haskell to reduce lines

--- Problem_1/Problem_1.hs
+++ Problem_1/Problem_1.hs
@@ -2,8 +2,6 @@
 -- Find the sum of all the multiples of 3 or 5 below 1000.
 
 f x y = x + y
-sumOfThrees = sum [ x | x <- [1..999], x `mod` 3 == 0 ]
-sumOfFives = sum [ x | x <- [1..999], x `mod` 3 /= 0, x `mod` 5 == 0 ]
 
 main = do
-	print $ f sumOfThrees sumOfFives
+	print $ f (sum [ x | x <- [1..999], x `mod` 3 == 0 ]) (sum [ x | x <- [1..999], x `mod` 3 /= 0, x `mod` 5 == 0 ])
